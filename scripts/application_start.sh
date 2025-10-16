6bf1484ccaa42fdc355938733ba377e76f62d7e2
Scott Walters <scottw567@gmail.com>
2025-10-16 18:30:52 -0400

    changed application_start.sh to assign node env

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/campplanner-backend
-nohup node src/app.js > /dev/null 2>&1 &
+
+# Export NODE_ENV so your app knows it’s production
+export NODE_ENV=production
+
+# Run the app with nohup and log output
+nohup node src/app.js > app.log 2>&1 &
+echo "App started in background. Logs: /home/ubuntu/campplanner-backend/app.log"
--- src/util/logger.js
+++ src/util/logger.js
@@ -9,7 +9,7 @@ const logger = createLogger({
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: "appLog.log" }), 
+    new transports.File({ filename: "backendLog.log" }), 
   ],
 });
 
