842a58461e81f9d588e948de609446de941ac9ee
Scott Walters <scottw567@gmail.com>
2025-10-16 18:53:53 -0400

    fixed app

--- src/app.js
+++ src/app.js
@@ -84,8 +84,8 @@
     );
     process.env.AWS_REGION = await getParameter("/campplanner/AWS_REGION");
     process.env.CAMPPLANNER_TABLE = await getParameter("/campplanner/CAMPPLANNER_TABLE");
-    console.log("AWS region: ", AWS_REGION);
-    console.log("table: ", CAMPPLANNER_TABLE);
+    console.log("AWS region: ", process.env.AWS_REGION);
+    console.log("table: ", process.env.CAMPPLANNER_TABLE);
   } else {
     // Local: load from .env
     require("dotenv").config({ path: "./.env", override: true });
